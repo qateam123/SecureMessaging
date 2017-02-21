fa24a34af7c0cd1745dd2058b320c26b76f400a0
gemmai95 <gemma.i_95@hotmail.co.uk>
2017-02-21 15:12:07 +0000

    hello world and username

--- app/views/views.py
+++ app/views/views.py
@@ -1,7 +1,29 @@
 from app import app
-from flask import request
+from flask import request, redirect
+from flask import render_template, jsonify
+import sys
+
 
 @app.route('/')
 @app.route('/index')
 def index():
     return "Hello, World!"
+
+@app.route('/dev', methods=['GET', 'POST'])
+def devMode():
+    if request.method == 'POST':
+        user = request.form
+        username = user.get("username")
+        return redirect("/validate?username=" + username)
+    return render_template("dev.html")
+
+@app.route('/validate', methods=['GET'])
+def validate():
+    username = request.args.get('username')
+    if not username:
+        print ("No username", file=sys.stderr)
+        return jsonify(usernameValid = False)
+    else:
+        print (username, file=sys.stderr)
+        print ("fun", file=sys.stderr)
+        return jsonify(usernameValid = True)
---
+++ tests/unittest.py
@@ -0,0 +1,21 @@
+import unittest
+import os
+import sys
+#from app.views import views
+from flask.app.views import views
+#from .app import app
+
+from flask import request
+
+class FlaskTestCase(unittest.TestCase):
+
+    def test_username_valid():
+        response = redirect("/validate?username=" + "username")
+        assertEqual(json.loads(response.data), {"username": True})
+
+    def test_username_invalid():
+        response = redirect("/validate?username=")
+        assertEqual(json.loads(response.data), {"username": False})
+
+if __name__ == '__main__':
+  unittest.main()
