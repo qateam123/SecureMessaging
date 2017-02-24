e17e60376b473362376530444ffddf3ff2fe6116
tej99 <tejas.patel@qa.com>
2017-02-24 09:11:58 +0000

    travis temp fix

--- tests/my_tests.py
+++ tests/my_tests.py
@@ -1,6 +1,6 @@
 import unittest
-#import sys
-#sys.path.append('../../SecureMessaging')
+import sys
+sys.path.append('../secure-messaging-ui') 
 
 from app import app
 
@@ -11,7 +11,7 @@ class FlaskTestCase(unittest.TestCase):
     def test_msg_valid(self):
         tester = app.test_client(self)
         response = tester.get("/validate?msg=" + "msg", content_type='application/json')
-        self.assertEqual(json.loads(response.data), {"msgValid": True, 'msg': 'msg'})
+        self.assertEqual(json.loads(response.data), {"msgValid": True, 'msg': 'msg' })
 
     def test_msg_invalid(self):
         tester = app.test_client(self)
