5462819b3ae1b44bb43be71637b7681897bc26f2
gemmai95 <gemma.i_95@hotmail.co.uk>
2017-03-08 11:42:28 +0000

    fix a few kinks

--- tests/behave-BDD-tests/features/steps/messageEndpoint.py
+++ tests/behave-BDD-tests/features/steps/messageEndpoint.py
@@ -9,19 +9,19 @@ def step_impl(context):
     context.new_message = {
         "to": "emilio.ward@qa.com",
         "from": "emilio.ward@ons.gov.uk",
-        "message": "testing 1, 2, 3"
+        "body": "testing 1, 2, 3"
     }
 
 
 @when("a request for a new message is submitted")
 def step_impl(context):
-    message_endpoint = "http://172.28.93.106:5050/message"
+    message_endpoint = "http://localhost:5050/message"
     print("The url to go to is: " + message_endpoint)
     headers = {'Content-type': 'application/json'}
     context.response = requests.post(message_endpoint, json=context.new_message, headers=headers)
 
-    assert context.response.status_code == 201
-    assert context.response.headers['Content-Type'] == 'text/html; charset=utf-8'
+    assert context.response.status_code == 200
+    assert context.response.headers['Content-Type'] == 'application/json'
 
 
 @then('the message is created successfully, reached API endpoint')
