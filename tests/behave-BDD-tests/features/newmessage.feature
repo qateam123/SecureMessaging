589b663bf7a19ab6957c1a48e2e05b7708d5d5be
gemmai95 <gemma.i_95@hotmail.co.uk>
2017-03-09 08:42:23 +0000

    add behaviour tests for new message

--- tests/behave-BDD-tests/features/newmessage.feature
+++ tests/behave-BDD-tests/features/newmessage.feature
@@ -3,4 +3,4 @@ Feature: New message to endpoint
     Scenario: Ensure message sent to API endpoint
         Given a new message is created
          When a request for a new message is submitted
-         Then the message is created successfully, reached API endpoint
+         Then Successful response from API
---
+++ tests/behave-BDD-tests/features/steps/message.py
@@ -0,0 +1,27 @@
+from behave import *
+from selenium import webdriver
+
+driver = webdriver.Chrome()
+driver.get("http://localhost:5000/new-message")
+# Scenario: Ensure message sent to API endpoint
+
+@given('A new message is created')
+def step_impl(context):
+    from_input = driver.find_element_by_id("from_input")
+    from_input.send_keys("gemma.irving@ons.gov.uk")
+    to_input = driver.find_element_by_id("to_input")
+    to_input.send_keys("gemma.irving@qa.com")
+    message_input = driver.find_element_by_id("message_input")
+    message_input.send_keys("test, test, test")
+
+@when("a request for a new message is submitted")
+def step_impl(context):
+    driver.find_element_by_id("submit").click()
+
+@then("Successful response from API")
+def step_impl(context):
+    try:
+        driver.find_element_by_id("bubbles")
+        pass
+    except NoSuchElementException:
+        return False
