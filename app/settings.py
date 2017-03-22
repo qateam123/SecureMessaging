5033236a0eda9385c597f20f776f1fa87a5278d8
MIslam92 <Mohammad.Islam@qa.com>
2017-03-22 08:45:43 +0000

    U.I. additions

--- app/settings.py
+++ app/settings.py
@@ -1,3 +1,9 @@
 import os
 
 SECURE_MESSAGING_API_URL = os.getenv('SECURE_MESSAGING_API_URL', "http://localhost:5050/message")
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.10/howto/static-files/
+
+STATIC_URL = '/static/'
+MAX_CHARS = 1200
